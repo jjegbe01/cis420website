8de1d6e278c6408e5b7a7e25a604c29bde26e32c
Vittitow <corey.vittitow@gmail.com>
2015-02-28 00:20:37 -0500

    Group Based Roles

--- University of Louisville/Vaccines and Travel Clinic/App_Start/IdentityConfig.cs
+++ University of Louisville/Vaccines and Travel Clinic/App_Start/IdentityConfig.cs
@@ -102,9 +102,9 @@ namespace IdentitySample.Models
     }
 
     // This is useful if you do not want to tear down the database each time you run the application.
-    // public class ApplicationDbInitializer : DatabaseAlways<ApplicationDbContext>
+    // public class ApplicationDbInitializer : DropCreateDatabaseAlways<ApplicationDbContext>
     // This example shows you how to create a new database if the Model changes
-    public class ApplicationDbInitializer : DropCreateDatabaseIfModelChanges<ApplicationDbContext> 
+    public class ApplicationDbInitializer : DropCreateDatabaseAlways<ApplicationDbContext> 
     {
         protected override void Seed(ApplicationDbContext context) {
             InitializeIdentityForEF(context);
@@ -128,16 +128,18 @@ namespace IdentitySample.Models
 
             var user = userManager.FindByName(name);
             if (user == null) {
-                user = new ApplicationUser { UserName = name, Email = name };
+                user = new ApplicationUser { UserName = name, Email = name, EmailConfirmed = true };
                 var result = userManager.Create(user, password);
                 result = userManager.SetLockoutEnabled(user.Id, false);
+
             }
 
-            // Add user admin to Role Admin if not already added
-            var rolesForUser = userManager.GetRoles(user.Id);
-            if (!rolesForUser.Contains(role.Name)) {
-                var result = userManager.AddToRole(user.Id, role.Name);
-            }
+            var groupManager = new ApplicationGroupManager();
+            var newGroup = new ApplicationGroup("SuperAdmins", "Full Access to All");
+
+            groupManager.CreateGroup(newGroup);
+            groupManager.SetUserGroups(user.Id, new string[] { newGroup.Id });
+            groupManager.SetGroupRoles(newGroup.Id, new string[] { role.Name });
         }
     }
 
