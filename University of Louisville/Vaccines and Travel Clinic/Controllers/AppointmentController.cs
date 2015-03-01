6d7f872ed83a5053eef8f7b718fdbc96d671cf78
maseba01 <maseba01@louisville.edu>
2015-03-01 18:08:08 -0500

    initial working calendar

---
+++ University of Louisville/Vaccines and Travel Clinic/Controllers/AppointmentController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Vaccines_and_Travel_Clinic.Controllers
+{
+    public class AppointmentController : Controller
+    {
+        // GET: Appointment
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
