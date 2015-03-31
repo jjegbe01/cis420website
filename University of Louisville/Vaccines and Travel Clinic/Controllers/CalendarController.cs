f3e49b99b67a7704558340d30a3c2dd398eeeee7
was2792 <was.shelton@gmail.com>
2015-03-30 21:07:58 -0400

    Further Scheduler Updates
    updates

--- University of Louisville/Vaccines and Travel Clinic/Controllers/CalendarController.cs
+++ University of Louisville/Vaccines and Travel Clinic/Controllers/CalendarController.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DHTMLX.Scheduler.Controls;
 
+using Vaccines_and_Travel_Clinic.DAL;
 using System.Data;
 using System.Data.Entity;
 using DHTMLX.Helpers;
 using DHTMLX.Scheduler;
 using DHTMLX.Scheduler.Data;
 using DHTMLX.Common;
+using DHTMLX.Scheduler.Settings;
+
+
+
+
 
 
 using Vaccines_and_Travel_Clinic.Models;
@@ -18,14 +25,56 @@ namespace Vaccines_and_Travel_Clinic.Controllers
 {
     public class CalendarController : Controller
     {
-        
-
+       
         private CalendarContext db = new CalendarContext();
-
+        private ClinicContext CC = new ClinicContext();
+        
         // GET: Calendar
         public ActionResult Index()
         {
+
+            //Initialize scheduler dropdowns and agenda view
             var scheduler = new DHXScheduler(this);
+            var patientDropDown = new LightboxSelect("patient", "Patient ID");
+            var roomDropDown = new LightboxSelect("room", "Room Number");
+            var agenda = new AgendaView();
+
+            //Add agenda view
+            scheduler.Views.Add(agenda);
+
+            //Create room list
+            var items = new List<object>(){
+                 new {key = "1", label = "Room 1"},
+                   new {key = "2", label = "Room 2"},
+                     new {key = "3", label = "Room 3"},
+                       new {key = "4", label = "Consultation Room"}
+             };
+
+            //create Patient ID List
+            var tempPeeps = new List<object>(){
+                new {key = "5", label = "1"},
+                new {key = "6", label = "2"},
+                new {key = "7", label = "3"},
+                new {key = "8", label = "3"},
+                new {key = "9", label = "4"},
+                new {key = "10", label = "5"}
+            };
+
+
+           
+
+            scheduler.Templates.agenda_text = "{text}";
+
+
+            patientDropDown.AddOptions(tempPeeps);
+            roomDropDown.AddOptions(items);
+            
+            
+           
+            scheduler.Lightbox.AddDefaults();
+            scheduler.Lightbox.Add(patientDropDown);
+            scheduler.Lightbox.Add(roomDropDown);
+            
             
             scheduler.Skin = DHXScheduler.Skins.Flat;
             scheduler.Config.drag_resize = false;
@@ -41,7 +90,14 @@ namespace Vaccines_and_Travel_Clinic.Controllers
             scheduler.Config.auto_end_date = true;  
             scheduler.Config.first_hour = 8;
             scheduler.Config.last_hour = 17;
-             
+            scheduler.Config.hour_date = "%h:%i%A"; 
+           
+           
+            
+           
+            
+            
+            
            
             
             scheduler.EnableDynamicLoading(SchedulerDataLoader.DynamicalLoadingMode.Month);
---
+++ University of Louisville/packages/DHTMLX.Scheduler.MVC3.Sample.1.0.1/Content/Models/SchedulerModel.cs.pp
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace $rootnamespace$.Models
+{
+    public class CalendarEvent
+    {
+        //id, text, start_date and end_date properties are mandatory
+        public int id { get; set; }
+        public string text { get; set; }
+        public DateTime start_date { get; set; }
+        public DateTime end_date { get; set; }
+    }
+}
